fce0966aefe9f909f0f6e7ecff7b40bed3c9d144
Henry <siyu.han2805@gmail.com>
2024-06-11 20:27:15 +1000

    Introduce finalised version of rps game with working animations, logic and restart button

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 let humanScore, computerScore;
 humanScore = 0;
 computerScore = 0;
+//global variables to clone button nodes
+let btnNodes;
 
-
+//query selector get get an instance of the btn container 
+const btnContainer = document.querySelector(".btn-container");
 //query selector to get an instance of all 3 buttons
 const buttons = document.querySelectorAll('button');
 //query selector to get instance of left and right arms
@@ -24,14 +27,21 @@ buttons.forEach((button) => {
     button.addEventListener("click", () => {
         setTimeout(() => {
             playRound(button.value, getComputerChoice());
+            //reinstate the buttons after round is finished and score is not 5
+            if (computerScore < 5 && humanScore < 5) {
+                enableBtns(btnNodes);
+            }
+            
         }, 2000);
+        //disable buttons
+        disableBtns();
         //animation for 2 secs
         leftArm.style.animation = "shakeAi 2s ease";
         rightArm.style.animation = "shakePlayer 2s ease";
         //set the arm images src back to the default src 
         resetArms();
-        
     });
+    
 });
 
 
@@ -72,6 +82,7 @@ const playRound = (humanChoice, computerChoice) => {
             leftArm.src = "./images/aiPaper.svg"
             console.log("You lose! " + computerChoice[0].toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice[0].toUpperCase() + humanChoice.slice(1));
             computerScore++;
+            
         }
         //win condition
         else if (computerChoice === "scissors") {
@@ -127,6 +138,8 @@ const playRound = (humanChoice, computerChoice) => {
     }
     //at round end display the updated score
     updateScore();
+    //check for any winners
+    determineWinner(humanScore, computerScore);
     
 } 
 
@@ -149,14 +162,115 @@ const updateScore = () => {
 
 //function to determine winner based on the humanScore and computerScore arguments
 const determineWinner = (humanScore, computerScore) => {
-    if (humanScore > computerScore) {
+    if (humanScore === 5) {
         console.log("The human wins!");
+        //create a new element
+        const element = document.createElement("h1");
+        element.textContent = "You Win!";
+        element.setAttribute("style", "font-size: 9.6rem; text-shadow: 4px 4px 0px #000; -webkit-text-stroke-width: .1rem;  -webkit-text-stroke-color: #000;");
+        //replace the buttons with the outcome text
+        btnContainer.appendChild(element);
+        //create the button element to restart the game
+        const btn = document.createElement("button");
+        //create the img element to attach to the button
+        const btnCover = document.createElement("img");
+        btnCover.src = "./images/restartBtn.svg";
+        btnContainer.setAttribute("style", "flex-direction: column; text-align: center; gap: 1.6rem;");
+        //add image tag as a child to the button parent
+        btn.appendChild(btnCover);
+        //adding css attributes to btn
+        btn.classList.add("btn");
+        //add the button element as a child to the btn-container class
+        btnContainer.appendChild(btn);
+        //add restart logic
+        btn.addEventListener("click", () => {
+            location.reload();
+        });
+
+        //animate background image with the scale-in effect of winning colour
+        //create a style element to add to the head
+        const style = document.createElement("style");
+        style.textContent = `
+            body::before {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                z-index: -1;
+                background-color: #A4EE98;
+                animation: scaleInWin 500ms ease 0s 1 normal forwards
+            }
+        `
+        document.head.appendChild(style);
+        
     }
-    else if (computerScore > humanScore) {
-        console.log("The machine wins!")
-    }
-    else {
-        console.log("It's a tie!");
+    else if (computerScore === 5) {
+        console.log("AI wins!");
+        //create a new element
+        const element = document.createElement("h1");
+        element.textContent = "You Lose!";
+        element.setAttribute("style", "font-size: 9.6rem; text-shadow: 4px 4px 0px #000; -webkit-text-stroke-width: .1rem;  -webkit-text-stroke-color: #000;");
+        //replace button with the outcome text
+        btnContainer.appendChild(element);
+        //create the button element to restart the game
+        const btn = document.createElement("button");
+        //create the img element to attach to the button
+        const btnCover = document.createElement("img");
+        btnCover.src = "./images/restartBtn.svg";
+        btnContainer.setAttribute("style", "flex-direction: column; text-align: center; gap: 1.6rem;");
+        //add image tag as a child to the button parent
+        btn.appendChild(btnCover);
+        //adding css attributes to btn
+        btn.classList.add("btn");
+        //add restart logic to button
+        btn.addEventListener("click", () => {
+                location.reload();
+        });
+        //add the button element as a child to the btn-container class
+        btnContainer.appendChild(btn);
+        
+
+
+        //animate background image with the scale-in effect of winning colour
+        //create a style element to add to the head
+        const style = document.createElement("style");
+        style.textContent = `
+            body::before {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                z-index: -1;
+                background-color: #FF9DE9;
+                animation: scaleInWin 500ms ease 0s 1 normal forwards
+            }
+        `
+        document.head.appendChild(style);
     }
 }
+
+//function to disable buttons when the animation is running 
+const disableBtns = () => {
+    //clone a list of all children nodes
+    btnNodes = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        //remove the btns children from the parent
+        btnContainer.removeChild(button);
+    });
+}
+
+//function to enable buttons when animation is finished 
+const enableBtns = (nodes) => {
+    //append the nodes back to the parent node
+    nodes.forEach((node) => {
+        btnContainer.appendChild(node);
+    });
+}
+
+
+
 
